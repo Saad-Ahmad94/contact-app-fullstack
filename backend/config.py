fd29232c052d7ff24fe58a8e73a3275cecfef0bd
Saad Ahmad <AHM@spacetec.local>
2025-06-11 17:44:41 +0200

    Adding postgre db and Docker files

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 
 
@@ -9,7 +10,7 @@ app = Flask(__name__)
 CORS(app)
 
 #Specifying location of db which will be of type sqlite and stored as a file
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db" 
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://user:password@db:5432/contacts_db"
 
 #No need to track modifications in database
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
--- backend/main.py
+++ backend/main.py
@@ -60,6 +60,8 @@ def delete_contact(user_id):
 
 if __name__ == '__main__':
   with app.app_context():
+    print("Hello Saad creating empty table")
     db.create_all()
+  
+  app.run(host='0.0.0.0', port=5000, debug=True)
 
-  app.run(debug=True)
